de99b2f5270d079999806e79072714de497ad2b5
HizanLi <L2542293790@gmail.com>
2023-10-10 22:47:49 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip3 install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install pillow==4.1.0
 pip install mmcls==0.25.0 openmim scipy scikit-learn ftfy regex tqdm
 mim install mmcv-full==1.6.0
