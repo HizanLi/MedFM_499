68bf305eff52fbbf25926b2c2dc43a350ad6cdd1
HizanLi <80601415+HizanLi@users.noreply.github.com>
2023-10-11 03:03:31 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install pillow==4.1.0
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install mmcls==0.25.0 openmim scipy scikit-learn ftfy regex tqdm
 mim install mmcv-full==1.6.0
